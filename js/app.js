f31a4f62b83046a6c32604468bc3c9c743cbeb4f
Danilo Jr <danilobjr@gmail.com>
2012-03-02 17:23:31 -0300

    Implementando instalacao GCF

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
+var hideAnimationContainer = function () {
+    var container = document.getElementById('animation');
+    container.style.cssText = 'display: none;';
+};
+
+var showAnimationContainer = function () {
+    var container = document.getElementById('animation');
+    container.style.cssText = 'display: block;';
+};
+
 window.onload = function () {
 
     CFInstall.check({
         mode: "overlay",
-        destination: "http://danilojrr.github.com/ChromeFrameDetector"
+        destination: "http://danilojrr.github.com/ChromeFrameDetector",
+        //preventPrompt: true,
+        onmissing: function () {
+            hideAnimationContainer();
+        },
+        oninstall: function () {
+            showAnimationContainer();
+        }
     });
     
 };
